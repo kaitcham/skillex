7f5d17c1fb255e29657f3488944abf0d00d9434c
Munezero Christian <chamsf5@aol.com>
2023-03-16 19:18:37 +0200

    Add NavLinks

---
+++ src/utils/data.jsx
@@ -0,0 +1,22 @@
+export const NavLinks = [
+  {
+    id: 1,
+    name: 'About',
+    path: 'about',
+  },
+  {
+    id: 2,
+    name: 'Courses',
+    path: 'courses',
+  },
+  {
+    id: 3,
+    name: 'Teachers',
+    path: 'teachers',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    path: 'contact',
+  },
+];
