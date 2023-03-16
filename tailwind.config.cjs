2be9b083ed8a55e8f890702dd3c1840b34fe42b3
Munezero Christian <chamsf5@aol.com>
2023-03-16 19:16:17 +0200

    Extend tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      Poppins: 'Poppins',
+    },
+    extend: {
+      colors: {
+        teal: '#2F6C6D',
+        hummingBird: '#d1f1ee',
+        yellow: '#e4d63b',
+        solitude: '#e9e9ea',
+        gray: '#4B4B4C',
+      },
+      animation: {
+        slide: 'slide 25s linear infinite',
+      },
+      keyframes: {
+        slide: {
+          '0%,100%': { transform: 'translateX(5%)' },
+          '50%': { transform: 'translateX(-120%)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
