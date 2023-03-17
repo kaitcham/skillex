68aa7be65d2a36419c188d00169d597a8140d350
Munezero Christian <chamsf5@aol.com>
2023-03-17 10:00:17 +0200

    Display landing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Navbar } from './components/_index';
+import { LandingSection } from './sections/_index';
 
 const App = () => (
   <div className="bg-solitude font-Poppins">
     <Navbar />
+    <LandingSection />
   </div>
 );
 
