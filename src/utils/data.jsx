498e3828730710c8378ccf285e1770ef2c4cbe7f
Munezero Christian <chamsf5@aol.com>
2023-03-17 10:01:24 +0200

    Import and export logos

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,3 +1,12 @@
+import logo1 from '../assets/logo1.png';
+import logo2 from '../assets/logo2.png';
+import logo3 from '../assets/logo3.png';
+import logo4 from '../assets/logo4.png';
+import logo5 from '../assets/logo5.png';
+import logo6 from '../assets/logo6.png';
+
+export const logos = [logo1, logo2, logo3, logo4, logo5, logo6];
+
 export const NavLinks = [
   {
     id: 1,
